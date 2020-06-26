b235a45388835dd0300ae91a11a0fb5f03f4aea3
joeliselin <56974093+joeliselin@users.noreply.github.com>
2020-06-26 13:34:44 +0200

    implementing delete methode for favourite

--- src/routes/favourite.js
+++ src/routes/favourite.js
@@ -5,15 +5,6 @@ const favourite = require('../models/favourite');
 /**
 * Return all Favourites
 */
-// router.get('/favorite', (req, res) => {
-//     favourite.find().then(dbFavourite => {
-//         console.log(dbFavourite);
-//         res.status(200).json({
-//             message: "fetched succssfully",
-//             Posts: dbFavourite
-//         });
-//     });
-// });
 
 router.get('/favorite', (req, res) => {
     favourite.find({} ,function (err, result) {
@@ -41,15 +32,16 @@ router.post('/favorite', (req, res) => {
 });
 
 
-// /**
-//  * Delete Favorite by id
-//  */
-// router.delete('/favorite/:id', (req, res) => {
-//     const query = { _id: new mongodb.ObjectID(req.params.id) };
-//     collectionFavourite.deleteOne(query, function(err, obj) {
-//         if (err) throw err;
-//         res.send({result: 'favorite deleted'});
-//     });
-// });
+/**
+ * Delete Favorite by id
+ */
+router.delete('/favorite/:id', (req, res) => {
+    favourite.findByIdAndDelete(req.params.id, function (err) {
+        if(err) console.log(err);
+        console.log("Successful deletion");
+        res.send({result: 'favorite deleted'});
+    });
+});
+
 
 module.exports = router;
